5048e4204e1d9891c04132e96bfccb03086140fe
romue <robert.mueller@campus.lmu.de>
2021-11-11 12:45:28 +0100

    adjusted setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
-# setup.py
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    reqs = [line.strip() for line in f]
 
 setup(
-    name='F_IKS',
-    packages=find_packages()
+    name="EDYs",
+    version="0.1",
+    python_requires=">=3.6",
+    packages=find_packages(),
+    install_requires=reqs,
 )
