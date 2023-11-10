3d18fe98390e0b618f1c589e3a4e1a37649cd3d9
Chanumask <joelfriedrich@gmx.de>
2023-11-10 10:43:11 +0100

    added test hooks (like rules)

---
+++ marl_factory_grid/environment/tests.py
@@ -0,0 +1,42 @@
+import abc
+from typing import List
+
+from marl_factory_grid.utils.results import TickResult, DoneResult
+
+
+class Test(abc.ABC):
+
+    @property
+    def name(self):
+        return self.__class__.__name__
+
+    def __init__(self):
+        pass
+
+    def __repr__(self):
+        return f'{self.name}'
+
+    def on_init(self, state, lvl_map):
+        return []
+
+    def on_reset(self):
+        return []
+
+    def tick_pre_step(self, state) -> List[TickResult]:
+        return []
+
+    def tick_step(self, state) -> List[TickResult]:
+        return []
+
+    def tick_post_step(self, state) -> List[TickResult]:
+        return []
+
+    def on_check_done(self, state) -> List[DoneResult]:
+        return []
+
+
+class FirstTest(Test):
+
+    def __init__(self):
+        super().__init__()
+        pass
---
+++ marl_factory_grid/testing/test_run.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from random import randint
+
+from tqdm import trange
+
+from marl_factory_grid.environment.factory import Factory
+
+if __name__ == '__main__':
+    # Render at each step?
+    render = True
+
+    # Path to config File
+    path = Path('test_config.yaml')
+
+    # Env Init
+    factory = Factory(path)
+
+    for episode in trange(5):
+        _ = factory.reset()
+        done = False
+        if render:
+            factory.render()
+        action_spaces = factory.action_space
+        while not done:
+            a = [randint(0, x.n - 1) for x in action_spaces]
+            obs_type, _, _, done, info = factory.step(a)
+            if render:
+                factory.render()
+            if done:
+                print(f'Episode {episode} done...')
+                break
+
