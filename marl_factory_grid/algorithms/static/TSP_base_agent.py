3cd5caed9343802efc0c357b8ba9ce6c6996d47b
Joel Friedrich <joelfriedrich@gmx.de>
2024-01-04 17:48:32 +0100

    added documentation for agents

--- marl_factory_grid/algorithms/static/TSP_base_agent.py
+++ marl_factory_grid/algorithms/static/TSP_base_agent.py
@@ -17,6 +17,16 @@ future_planning = 7
 class TSPBaseAgent(ABC):
 
     def __init__(self, state, agent_i, static_problem: bool = True):
+        """
+        Abstract base class for agents in the environment.
+
+        :param state: The environment state
+        :type state:
+        :param agent_i: Index of the agent
+        :type agent_i: int
+        :param static_problem: Indicates whether the TSP is a static problem. (Default: True)
+        :type static_problem: bool
+        """
         self.static_problem = static_problem
         self.local_optimization = True
         self._env = state
@@ -26,9 +36,25 @@ class TSPBaseAgent(ABC):
 
     @abstractmethod
     def predict(self, *_, **__) -> int:
+        """
+        Predicts the next action based on the environment state.
+
+        :return: Predicted action.
+        :rtype: int
+        """
         return 0
 
     def _use_door_or_move(self, door, target):
+        """
+        Helper method to decide whether to use a door or move towards a target.
+
+        :param door: Door entity.
+        :type door: Door
+        :param target: Target type. For example 'Dirt', 'Dropoff' or 'Destination'
+        :type target: str
+
+        :return: Action to perform (use door or move).
+        """
         if door.is_closed:
             # Translate the action_object to an integer to have the same output as any other model
             action = do.ACTION_DOOR_USE
@@ -37,6 +63,15 @@ class TSPBaseAgent(ABC):
         return action
 
     def calculate_tsp_route(self, target_identifier):
+        """
+        Calculate the TSP route to reach a target.
+
+        :param target_identifier: Identifier of the target entity
+        :type target_identifier: str
+
+        :return: TSP route
+        :rtype: List[int]
+        """
         positions = [x for x in self._env.state[target_identifier].positions if x != c.VALUE_NO_POS]
         if self.local_optimization:
             nodes = \
@@ -55,6 +90,15 @@ class TSPBaseAgent(ABC):
         return route
 
     def _door_is_close(self, state):
+        """
+        Check if a door is close to the agent's position.
+
+        :param state: Current environment state.
+        :type state: Gamestate
+
+        :return: Closest door entity or None if no door is close.
+        :rtype: Door | None
+        """
         try:
             return next(y for x in state.entities.neighboring_positions(self.state.pos)
                         for y in state.entities.pos_dict[x] if do.DOOR in y.name)
@@ -62,9 +106,27 @@ class TSPBaseAgent(ABC):
             return None
 
     def _has_targets(self, target_identifier):
+        """
+        Check if there are targets available in the environment.
+
+        :param target_identifier: Identifier of the target entity.
+        :type target_identifier: str
+
+        :return: True if there are targets, False otherwise.
+        :rtype: bool
+        """
         return bool(len([x for x in self._env.state[target_identifier] if x.pos != c.VALUE_NO_POS]) >= 1)
 
     def _predict_move(self, target_identifier):
+        """
+           Predict the next move based on the given target.
+
+           :param target_identifier: Identifier of the target entity.
+           :type target_identifier: str
+
+           :return: Predicted action.
+           :rtype: int
+           """
         if self._has_targets(target_identifier):
             if self.static_problem:
                 if not self._static_route:
