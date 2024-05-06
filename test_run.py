39b123221b8703baa10c4a2dde64f876d25b8afd
Chanumask <joelfriedrich@gmx.de>
2024-05-06 19:30:27 +0200

    cleanup

--- test_run.py
+++ test_run.py
@@ -41,4 +41,4 @@ if __name__ == '__main__':
                 print(f'Episode {episode} done...')
                 break
 
-        plot_routes(factory, agents, )
+        plot_routes(factory, agents)
