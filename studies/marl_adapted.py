4571dc1cd1c0f60e6c351c421ada1670da9e19b3
Julian Schönberger <schoenbergerj@cip.ifi.lmu.de>
2024-05-17 17:52:52 +0200

    Added shared piles option to dirt_quadrant eval + Changed dirt_quadrant layout and adapted configs

--- studies/marl_adapted.py
+++ studies/marl_adapted.py
@@ -31,7 +31,7 @@ def single_agent_eval(config_name, run):
     agent = A2C(train_cfg, eval_cfg)
     print("Evaluation phase")
     agent.load_agents(run)
-    agent.eval_loop(10)
+    agent.eval_loop(1)
 
 
 def multi_agent_eval(config_name, runs, emergent_phenomenon=False):
@@ -56,7 +56,7 @@ def multi_agent_eval(config_name, runs, emergent_phenomenon=False):
     agent = A2C(train_cfg, eval_cfg)
     print("Evaluation phase")
     agent.load_agents(runs)
-    agent.eval_loop(10)
+    agent.eval_loop(1)
 
 
 def dirt_quadrant_single_agent_training():
@@ -71,7 +71,7 @@ def dirt_quadrant_single_agent_eval(agent_name):
     if agent_name == "Sigmund":
         run = "run0"
     elif agent_name == "Wolfgang":
-        run = "run4"
+        run = "run1"
     single_agent_eval("dirt_quadrant", [run])
 
 
@@ -83,14 +83,14 @@ def two_rooms_one_door_modified_single_agent_eval(agent_name):
     single_agent_eval("two_rooms_one_door_modified", [run])
 
 
-def dirt_quadrant_multi_agent_eval(emergent_phenomenon):
-    multi_agent_eval("dirt_quadrant", ["run0", "run1"], emergent_phenomenon)
+def dirt_quadrant_5_multi_agent_eval(emergent_phenomenon):
+    multi_agent_eval("dirt_quadrant", ["run4", "run5"], emergent_phenomenon)
 
+def dirt_quadrant_5_multi_agent_ctde_eval(emergent_phenomenon): # run7 == run4
+    multi_agent_eval("dirt_quadrant", ["run4", "run7"], emergent_phenomenon)
 
 def two_rooms_one_door_modified_multi_agent_eval(emergent_phenomenon):
     multi_agent_eval("two_rooms_one_door_modified", ["run2", "run3"], emergent_phenomenon)
-
-
 
 
 if __name__ == '__main__':
